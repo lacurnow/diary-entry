e32949d736cae6c9e5fb7b834c690ef826d43427
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-08 15:49:47 +0000

    99% done

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -23,12 +23,14 @@ class DiaryEntry
 
   def reading_chunk(wpm, minutes) 
     words_in_time = wpm * minutes
-    chunk = contents.split[@cursor...words_in_time].join(" ")
+    p @cursor
+    p end_cursor = (words_in_time + @cursor)
+    if count_words > end_cursor
+
+    end
+    chunk = contents.split[@cursor...end_cursor].join(" ")
     @cursor += words_in_time
-    chunk
-    
-    
-    
+    p chunk
     
     
     # `wpm` is an integer representing the number
@@ -42,3 +44,10 @@ class DiaryEntry
     # The next call after that it should restart from the beginning.
   end
 end
+
+
+entry = DiaryEntry.new("Diary", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate congue rutrum. Fusce suscipit tortor ex, et sodales leo aliquam ut. Suspendisse fermentum est vel augue egestas, et fringilla mauris varius. Pellentesque in pharetra dui. Sed commodo nulla nec rhoncus elementum. Praesent vitae aliquet dolor, ac lobortis mauris. Maecenas sit amet dolor pharetra, suscipit magna quis, ornare elit. Aenean varius ullamcorper lorem eget varius. Etiam lacinia felis ligula, ut faucibus ligula vulputate id. Aenean non accumsan nulla. Quisque non facilisis sapien. Pellentesque dapibus, tortor sit amet commodo accumsan, sem tortor tincidunt metus, eu pulvinar eros quam a elit. Donec consectetur nisl ut enim tristique, vel congue orci vulputate. Aliquam sed ullamcorper ante, id lobortis turpis. Nunc urna lorem, convallis et varius eu, tempus non nisl. Donec a sem pharetra, ultricies ipsum id, hendrerit metus. Proin libero dui, scelerisque nec purus ac, malesuada fermentum elit. Nunc pellentesque blandit gravida. Nullam convallis, magna eu vulputate fermentum, nulla risus dignissim mi, eu pulvinar sem ex vel nulla. In est sapien, pellentesque sit amet faucibus eu, egestas in mauris. Sed vestibulum sapien arcu, id consequat tellus commodo fermentum. Donec eu tincidunt nunc, vitae rhoncus elit. Aenean varius enim arcu, a sollicitudin libero lacinia quis.")
+entry.reading_chunk(200, 1)
+entry.reading_chunk(200, 1)
+entry.reading_chunk(200, 1)
+entry.reading_chunk(200, 1)
